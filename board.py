f0482365ac0e6f87876dce8d05b5c610e073319d
anomius <swarajpurohit@gmail.com>
2021-01-24 00:53:41 +0530

    added board

--- board.py
+++ board.py
@@ -1,4 +1,7 @@
 import Cell
+import os
+from random import randint
+
 
 class board:
     def __init__(self,rows=None,cols=None) -> None:
@@ -11,5 +14,17 @@ class board:
         
         self.grid = [[Cell() for column_cells in range(self._col)] for row_cells in range(self._row)]
         
-        self._generate_board()
+        self.generate_board()
+
+    def draw_board(self):
+       print('\n'*10)
+       for row in self.grid:
+           for column in row:
+               print(column,end="")
         
+    def generate_board():
+        for row in self._grid:
+            for column in row:
+                ran = randint(0,2)
+                if ran==1:
+                    column.set_liv
